76443672c5e6caa5dd361168ae095358a22d19dc
Cisne Parra <adparra2@utpl.edu.ec>
2023-11-29 16:03:03 -0500

    cambios de la clase

---
+++ problema4/src/problema4/Problema4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema4;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Problema4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 2;
+        while (n <= 3){
+            n = n + 1;
+        }
+        while (n <= 4){
+            n = n + 1;
+        }
+        System.out.printf("%d",n);
+    }
+    
+}
